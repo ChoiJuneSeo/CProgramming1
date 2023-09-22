4f493e2b94b755f9418b18cb1b4a89ddd05dcf2a
CJS_Labtop <cjs07310125@gmail.com>
2023-09-22 18:25:26 +0900

    3주차 과제

--- Hw2/hw2.c
+++ Hw2/hw2.c
@@ -1,1 +1,14 @@
-// TEST
+#include <stdio.h>
+
+
+int main()
+{
+	float input;
+
+	printf("Please enter kilometers:");
+	scanf_s("%f", &input);
+	float output = input / 1.609;
+	printf("%.1fkm is equal to %.1f miles\n", input, output);
+
+	return 0;
+}
